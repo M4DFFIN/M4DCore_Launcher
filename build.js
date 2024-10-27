64976d81fe5de96e6ffdfd4a6cfa54f77efa2860
M4DFFIN <79491351+M4DFFIN@users.noreply.github.com>
2024-10-26 22:09:44 -0300

    modified:   build.js

--- build.js
+++ build.js
@@ -65,7 +65,7 @@ class Index {
                 generateUpdatesFilesForAllChannels: false,
                 appId: preductname,
                 productName: preductname,
-                copyright: 'Copyright © 2020-2024 Luuxis',
+                copyright: 'Copyright © 2024 M4DFFIN',
                 artifactName: "${productName}-${os}-${arch}.${ext}",
                 extraMetadata: { main: 'app/app.js' },
                 files: ["app/**/*", "package.json", "LICENSE.md"],
@@ -111,9 +111,9 @@ class Index {
                 }
             }
         }).then(() => {
-            console.log('le build est terminé')
+            console.log('La build se ha completado')
         }).catch(err => {
-            console.error('Error during build!', err)
+            console.error('Error durante la compilación.', err)
         })
     }
 
@@ -139,9 +139,9 @@ class Index {
             fs.writeFileSync("src/assets/images/icon.icns", png2icons.createICNS(Buffer, png2icons.BILINEAR, 0));
             fs.writeFileSync("src/assets/images/icon.ico", png2icons.createICO(Buffer, png2icons.HERMITE, 0, false));
             fs.writeFileSync("src/assets/images/icon.png", Buffer);
-            console.log('new icon set')
+            console.log('Nuevo conjunto de iconos')
         } else {
-            console.log('connection error')
+            console.log('Error de conexión')
         }
     }
 }
